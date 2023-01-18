888f5fad3ed744550cd2a5a282cb7154800a5f0e
PavelGith <linevich.p.l@gmail.com>
2023-01-18 17:14:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+  let arr = [];
+  for (let elem of str) {
+    let elemIndex = brackets.indexOf(elem);
+    if (elemIndex % 2 === 0) {
+      if (elem === brackets[elemIndex + 1] && arr[arr.length - 1] === elemIndex) {
+        arr.pop();
+      } else {
+        arr.push(elemIndex);
+      }
+    } else {
+      if (arr.pop() !== elemIndex - 1) {
+        return false;
+      }
+    }
+  }
+  return arr.length === 0;
 }
